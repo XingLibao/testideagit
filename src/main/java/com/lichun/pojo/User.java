a194ec73c8a07a590d52d68abe3265e828f31d17
xinglibao <1277750893@qq.com>
2021-07-16 22:40:05 +0800

    second commit

--- src/main/java/com/lichun/pojo/User.java
+++ src/main/java/com/lichun/pojo/User.java
@@ -2,6 +2,7 @@ package com.lichun.pojo;
 
 public class User {
     private String name;
+    private  int age;
 
     public String getName() {
         return name;
@@ -9,5 +10,13 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
